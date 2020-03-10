1f1431a41fcc10e5717d478fb2ec6f3bcc304e08
DAntonenko <dm.antonenko1977@gmail.com>
2020-03-10 18:19:03 +0300

    header done, tabs are working

--- src/root.tsx
+++ src/root.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './finctrl.css';
 import {Authorization} from './routes/authorization/authorization';
 import { Users } from './routes/users/users';
+import { AdminPanel } from './routes/admin-panel/admin-panel';
 
 class Root extends Component {
   render() {
     return (
       <div>
         {/* <Authorization /> */}
-        <Users />
+        <AdminPanel />
+        {/* <Users /> */}
       </div>
     );
   };
--- src/routes/admin-panel/admin-panel.tsx
+++ src/routes/admin-panel/admin-panel.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './admin-panel.css';
 import { Button } from '../../components/controls/button/button';
+import { mergeClassNames } from '../../utils/merge-class-names/merge-class-names';
 
 export enum EVisibleTab {
   USERS = 0,
@@ -12,7 +13,7 @@ interface IAdminPanelState {
   visibleTab: EVisibleTab;
 }
 
-export class AdminPanel extends React.Component<IAdminPanelState> {
+export class AdminPanel extends React.Component<{}, IAdminPanelState> {
 
   public state: IAdminPanelState = {
     visibleTab: EVisibleTab.USERS
@@ -22,35 +23,63 @@ export class AdminPanel extends React.Component<IAdminPanelState> {
     return (
       <div className='admin-panel'>
         <header className='admin-panel__header'>
+          <p className='admin-panel__symbol'>A</p>
           <nav className='admin-panel__main-navigation'>
             <p
-              className='admin-panel__main-navigation-tab'
+              className={mergeClassNames([
+                'admin-panel__main-navigation-tab',
+                this.state.visibleTab === EVisibleTab.USERS ? 'admin-panel__main-navigation-tab--active' : '',
+              ])}
+              id='users-tab'
               onClick={this.onNavTabClick}
             >
               Пользователи
             </p>
             <p
-              className='admin-panel__main-navigation-tab'
+              className={mergeClassNames([
+                'admin-panel__main-navigation-tab',
+                this.state.visibleTab === EVisibleTab.ACCOUNTS_MONITORING ? 'admin-panel__main-navigation-tab--active' : '',
+              ])}
+              id='accounts-monitoring-tab'
+              onClick={this.onNavTabClick}
             >
               Мониторинг аккаунтов
             </p>
             <p
-              className='admin-panel__main-navigation-tab'
+              className={mergeClassNames([
+                'admin-panel__main-navigation-tab',
+                this.state.visibleTab === EVisibleTab.LEGAL_ENTITIES_LIST ? 'admin-panel__main-navigation-tab--active' : '',
+              ])}
+              id='legal-entities-list-tab'
+              onClick={this.onNavTabClick}
             >
               Список юрлиц аккаунта
             </p>
           </nav>
         </header>
         <main className='admin-panel__main-content'>
-          {this.state.visibleTab = EVisibleTab.ACCOUNTS_MONITORING ? <p>МОНЯ</p>: <p></p>}
+          {this.state.visibleTab === EVisibleTab.USERS && <p>УСЁР</p>}
+          {this.state.visibleTab === EVisibleTab.ACCOUNTS_MONITORING && <p>МОНЯ</p>}
+          {this.state.visibleTab === EVisibleTab.LEGAL_ENTITIES_LIST && <p>ЛЫЦЫ</p>}
         </main>
       </div>
     );
   }
 
-  private onNavTabClick = () => {
-    this.setState({
-      visibleTab: EVisibleTab.ACCOUNTS_MONITORING
-    });
+  private onNavTabClick = (event: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+    switch (event.currentTarget.id) {
+      case 'users-tab':
+        this.setState({visibleTab: EVisibleTab.USERS});
+        break;
+      case 'accounts-monitoring-tab':
+        this.setState({visibleTab: EVisibleTab.ACCOUNTS_MONITORING});
+        break;
+      case 'legal-entities-list-tab':
+        this.setState({visibleTab: EVisibleTab.LEGAL_ENTITIES_LIST});
+        break;
+      default:
+        this.setState({visibleTab: EVisibleTab.USERS});
+        break;
+    }
   }
 }
